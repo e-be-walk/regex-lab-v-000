07b7c9e1a5a79bc0ed8195552a5ff1fa56a99b87
Erin Walker <erinb0317@hotmail.com>
2018-02-01 22:07:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,8 +4,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/\w+un+ing\/)
-
+  text.match(/\w+un+ing/)
 end
 
 def words_five_letters_long(text)
