ab2297f3a7f615672f4a178d6f60c376459d6115
Erin Walker <erinb0317@hotmail.com>
2018-02-01 22:34:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,5 +22,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d{9}\b/)
+  if phone.match(/\d{9}\b/)
+    true
+  else
+    false
+  end
 end
